26ab41b0724476d44f4293123151e703f862d768
Josh Kellendonk <josh@colloco.com>
2019-12-15 13:18:08 -0700

    fix broken api test

--- src/wordpress/api.test.ts
+++ src/wordpress/api.test.ts
@@ -1,8 +1,8 @@
-import { Post } from "./post";
 import * as WPAPI from "wpapi";
+import { Item } from "./item";
 
 type PagesOfType<T> = { [x: string]: T[] };
-const postPages: PagesOfType<Post> = {
+const postPages: PagesOfType<Item> = {
   "1": [...Array(10).keys()].map(i => {
     return { id: i, slug: `slug-${i}` };
   }),
@@ -12,7 +12,7 @@ const postPages: PagesOfType<Post> = {
 };
 
 type IDsOfType<T> = { [x: string]: T };
-const posts: IDsOfType<Post> = {
+const posts: IDsOfType<Item> = {
   "31216": {
     id: 31216,
     slug: "slug-31216"
@@ -22,10 +22,10 @@ const posts: IDsOfType<Post> = {
 export const apiMock: WPAPI = ({
   posts() {
     return {
-      async page(page: number): Promise<Post[]> {
+      async page(page: number): Promise<Item[]> {
         return postPages[page] ?? [];
       },
-      async id(postID: number): Promise<Post> {
+      async id(postID: number): Promise<Item> {
         return posts[postID];
       }
     };
